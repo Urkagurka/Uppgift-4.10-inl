f26d177d65ed95350f5d55c2ee82659b7e5353ce
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 18:42:40 +0200

    Add project files.

---
+++ Uppgift 4.10 inl/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Uppgift_4._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många X per X grupp?");
+            int mängdX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O per O grupp?");
+            int mängdO = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många O mönster per rad?");
+            int mängdOgrupp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många rader?");
+            int mängdRADER = int.Parse(Console.ReadLine());
+
+            string Xgrupp = "";
+            string Ogrupp = "";
+            string RAD = "";
+
+            for (int i = 0; i < mängdX; i++)
+            {
+                Xgrupp = Xgrupp + "X";
+            }
+            for (int i = 0; i < mängdO; i++)
+            {
+                Ogrupp = Ogrupp + "O";
+            }
+            for (int i = 0; i < mängdOgrupp; i++)
+            {
+                RAD = RAD + Xgrupp + "-" + Ogrupp + "-";
+            }
+            RAD = RAD + Xgrupp;
+            for(int i = 0; i < mängdRADER; i++)
+            {
+                Console.WriteLine(RAD);
+            }
+        }
+
+    }
+
+}
